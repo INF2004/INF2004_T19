29d506da476b987afa137c7b8cc8c815680ef8b8
brianlee51 <2837256L@student.gla.ac.uk>
2023-10-20 14:18:01 +0800

    Initial commit for motor branch codes, not tested yet

--- maze.c
+++ maze.c
@@ -1,13 +1,3 @@
-/* 
-    Develop an application using FreeRTOS that contains a task 
-    that reads the temperature data from the RP2040's built-in temperature
-    sensor and sends it to two tasks every 1 second. The second task will 
-    perform a moving average on a buffer of ten data points, and the third task
-    will perform a simple averaging. Additionally, create a fourth task exclusively
-    for executing all the printf statements. No printf statements are allowed in
-    any other task.
-*/
-
 #include <stdio.h>
 
 #include "hardware/gpio.h"
